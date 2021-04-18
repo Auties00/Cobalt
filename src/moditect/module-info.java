1ce9178019aed435988d79313e541fc122baa14a
GedMarc <marc.magon@gmail.com>
2021-04-18 11:53:12 +0200

    Moditect a clean module info

---
+++ src/moditect/module-info.java
@@ -0,0 +1,14 @@
+module it.auties.whatsapp4j {
+    requires jakarta.activation;
+    requires jakarta.validation;
+    requires jakarta.xml.bind;
+    requires jakarta.websocket.api;
+
+    requires com.google.common;
+
+    requires com.fasterxml.jackson.databind;
+
+    requires java.prefs;
+    requires java.net.http;
+    requires io.github.classgraph;
+}
