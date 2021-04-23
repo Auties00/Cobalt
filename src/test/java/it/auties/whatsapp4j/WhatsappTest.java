92c792564e70c0f9c4f4ab230a40ab5f0e1fc8a0
Auties00 <alautiero@gmail.com>
2021-04-23 20:02:28 +0200

    Changed version to 2.0 Implemented a unit test

---
+++ src/test/java/it/auties/whatsapp4j/WhatsappTest.java
@@ -0,0 +1,44 @@
+package it.auties.whatsapp4j;
+
+import it.auties.whatsapp4j.api.WhatsappAPI;
+import it.auties.whatsapp4j.listener.WhatsappListener;
+import it.auties.whatsapp4j.response.impl.UserInformationResponse;
+import jakarta.validation.constraints.NotNull;
+import lombok.extern.java.Log;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.util.concurrent.CountDownLatch;
+
+/**
+ * A simple class to check that the library is working
+ */
+@Log
+public class WhatsappTest implements WhatsappListener {
+  private static @NotNull CountDownLatch latch;
+  private static @NotNull WhatsappAPI whatsappAPI;
+
+  @BeforeAll
+  public static void init(){
+    log.info("Initializing api to start testing...");
+    whatsappAPI = new WhatsappAPI();
+    latch = new CountDownLatch(1);
+  }
+
+  @Test
+  public void testConnectionFlowBasic() throws InterruptedException{
+      log.info("Registering listener...");
+      whatsappAPI.registerListener(this);
+      log.info("Connecting...");
+      whatsappAPI.connect();
+      latch.await();
+  }
+
+  @Override
+  public void onLoggedIn(UserInformationResponse info, boolean firstLogin) {
+    log.info("Connected!");
+    whatsappAPI.disconnect();
+    latch.countDown();
+  }
+}
