b1397b1df359d76cd789f758929cfbf4c4517806
Alessandro Autiero <alautiero@gmail.com>
2022-07-01 01:05:57 +0200

    Added tokens test

---
+++ src/test/java/it/auties/whatsapp/unit/TokensCollectorTest.java
@@ -0,0 +1,94 @@
+package it.auties.whatsapp.unit;
+
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
+import java.util.Objects;
+import java.util.regex.Pattern;
+
+import static java.net.http.HttpResponse.BodyHandlers.ofString;
+
+public class TokensCollectorTest {
+    private static final HttpClient HTTP_CLIENT = HttpClient.newBuilder()
+            .version(HttpClient.Version.HTTP_1_1)
+            .build();
+    private static final String SOURCE_NAME = "Tokens.java";
+    private static final String SOURCE_FILE = "C:\\Users\\alaut\\NativeWhatsapp4j\\src\\main\\java\\it\\auties\\whatsapp\\binary\\Tokens.java";
+    private static final String TOKEN_REGEX = "<script defer=\"defer\" src=\"/bootstrap_qr.([^\"]*).js\">";
+    private static final String SINGLE_BYTE_REGEX = "t.SINGLE_BYTE_TOKEN=\\[\"(.*?)\"]";
+    private static final String DICTIONARY_0_REGEX = "const n=\\[\"(.*?)\"]";
+    private static final String DICTIONARY_1_REGEX = "const r=\\[\"(.*?)\"]";
+    private static final String DICTIONARY_2_REGEX = "const i=\\[\"(.*?)\"]";
+    private static final String DICTIONARY_3_REGEX = "const a=\\[\"(.*?)\"]";
+
+    @Test
+    public void createClass() throws IOException, InterruptedException {
+        var javascriptSource = getJavascriptSource();
+        var singleByteToken = getSingleByteTokens(javascriptSource);
+        var doubleByteTokens = getDoubleByteTokens(javascriptSource);
+        var sourceFile = getSourceFile().formatted(singleByteToken, doubleByteTokens);
+        Files.writeString(Path.of(SOURCE_FILE), sourceFile, StandardOpenOption.CREATE,
+                StandardOpenOption.TRUNCATE_EXISTING);
+    }
+
+    private String getSingleByteTokens(String javascriptSource) {
+        return getTokens(javascriptSource, SINGLE_BYTE_REGEX);
+    }
+
+    private String getDoubleByteTokens(String javascriptSource) {
+        return "%s,%s,%s,%s".formatted(getTokens(javascriptSource, DICTIONARY_0_REGEX),
+                getTokens(javascriptSource, DICTIONARY_1_REGEX), getTokens(javascriptSource, DICTIONARY_2_REGEX),
+                getTokens(javascriptSource, DICTIONARY_3_REGEX));
+    }
+
+    private String getTokens(String source, String regex) {
+        return "\"%s\"".formatted(findResult(source, regex));
+    }
+
+    private String getSourceFile() throws IOException {
+        var sourceStream = ClassLoader.getSystemResourceAsStream(SOURCE_NAME);
+        Objects.requireNonNull(sourceStream, "Cannot find source resource at %s".formatted(SOURCE_NAME));
+        return new String(sourceStream.readAllBytes(), StandardCharsets.UTF_8);
+    }
+
+    private String getJavascriptSource() throws IOException, InterruptedException {
+        var whatsappRequest = createRequest("https://web.whatsapp.com");
+        var whatsappResponse = HTTP_CLIENT.send(whatsappRequest, ofString());
+        var token = findResult(whatsappResponse.body(), TOKEN_REGEX);
+        var sourceRequest = createRequest("https://web.whatsapp.com/bootstrap_qr.%s.js".formatted(token));
+        return HTTP_CLIENT.send(sourceRequest, ofString())
+                .body();
+    }
+
+    private String findResult(String input, String regex) {
+        return Pattern.compile(regex, Pattern.MULTILINE)
+                .matcher(input)
+                .results()
+                .map(e -> e.group(1))
+                .findFirst()
+                .orElseThrow();
+    }
+
+    private static HttpRequest createRequest(String url) {
+        return HttpRequest.newBuilder()
+                .uri(URI.create(url))
+                .header("Host", "web.whatsapp.com")
+                .header("Connection", "keep-alive")
+                .header("Pragma", "no-cache")
+                .header("Cache-Control", "no-cache")
+                .header("Sec-Fetch-Site", "same-origin")
+                .header("Sec-Fetch-Mode", "no-cors")
+                .header("Sec-Fetch-Dest", "empty")
+                .header("User-Agent",
+                        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36")
+                .GET()
+                .build();
+    }
+}
---
+++ src/test/resources/Tokens.java
@@ -0,0 +1,32 @@
+package it.auties.whatsapp.binary;
+
+import lombok.NonNull;
+import lombok.experimental.UtilityClass;
+
+import java.util.List;
+import java.util.regex.Pattern;
+
+@UtilityClass
+public class Tokens {
+    public final List<String> SINGLE_BYTE = List.of(%s);
+
+    public final List<String> DOUBLE_BYTE = List.of(%s);
+
+    public final List<Character> NUMBERS = List.of('0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '-', '.', '�', '�',
+            '�', '�');
+
+    public final List<Character> HEX = List.of('0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D',
+            'E', 'F');
+
+    public final String NUMBERS_REGEX = "[^0-9.-]+?";
+
+    public final String HEX_REGEX = "[^0-9A-F]+?";
+
+    public boolean noMatch(@NonNull String input, @NonNull String regex) {
+        return Pattern.compile(regex)
+                .matcher(input)
+                .results()
+                .findAny()
+                .isEmpty();
+    }
+}
