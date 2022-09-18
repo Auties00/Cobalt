944d165be5b834f95f01a6ef47eb8e4130b8c1f6
Alessandro Autiero <alautiero@gmail.com>
2022-09-18 16:37:13 +0200

    Release RC22

--- src/test/java/it/auties/whatsapp/ci/WaitTest.java
+++ src/test/java/it/auties/whatsapp/ci/WaitTest.java
@@ -1,32 +1,25 @@
 package it.auties.whatsapp.ci;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
-import it.auties.whatsapp.api.ErrorHandler;
 import it.auties.whatsapp.api.Whatsapp;
-import it.auties.whatsapp.model.contact.ContactJid;
-import it.auties.whatsapp.model.message.standard.TextMessage;
+import it.auties.whatsapp.model.info.MessageInfo;
 import it.auties.whatsapp.util.JacksonProvider;
+import lombok.SneakyThrows;
 import org.junit.jupiter.api.Test;
 
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.stream.Collectors;
-
-public class WaitTest {
+public class WaitTest implements JacksonProvider {
     @Test
     public void testForFiveMinutes() {
-        var options = Whatsapp.Options.defaultOptions()
-                .withErrorHandler(ErrorHandler.toTerminal());
-        var whatsapp = Whatsapp.lastConnection(options)
+        Whatsapp.lastConnection()
                 .addLoggedInListener(() -> System.out.println("Connected"))
-                .addChatsListener((api) -> System.out.println("Received " +  api.store().chats().size() + " chats"))
-                .addContactsListener((api) -> System.out.println("Received " +  api.store().contacts().size() + " contacts"))
-                .addChatMessagesListener((chat, complete) -> {
-                        System.out.println(chat.name() + " > " +  chat.messages().size());
-                })
+                .addNewMessageListener(WaitTest::logMessage)
                 .connect()
-                .join();
+                .join()
+                .await();
+    }
 
-        whatsapp.await();
+    @SneakyThrows
+    private static void logMessage(MessageInfo message) {
+        System.out.println(JSON.writerWithDefaultPrettyPrinter().writeValueAsString(message));
     }
 }
