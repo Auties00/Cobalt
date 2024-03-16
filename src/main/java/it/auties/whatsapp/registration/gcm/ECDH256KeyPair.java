129226ba97afbf2b675a67874daa835f4b14ed43
Alessandro Autiero <alautiero@gmail.com>
2024-03-16 21:16:03 +0100

    Right private key length

--- src/main/java/it/auties/whatsapp/registration/gcm/ECDH256KeyPair.java
+++ src/main/java/it/auties/whatsapp/registration/gcm/ECDH256KeyPair.java
@@ -7,6 +7,7 @@ import java.security.SecureRandom;
 import java.security.interfaces.ECPrivateKey;
 import java.security.interfaces.ECPublicKey;
 import java.security.spec.ECGenParameterSpec;
+import java.util.Arrays;
 
 record ECDH256KeyPair(
         byte[] publicKey,
@@ -14,7 +15,7 @@ record ECDH256KeyPair(
         ECPublicKey jcaPublicKey,
         ECPrivateKey jcaPrivateKey
 ) {
-    private static final int PUBLIC_KEY_PART_LENGTH = 32;
+    private static final int KEY_PART_LENGTH = 32;
 
     static ECDH256KeyPair random() {
         try {
@@ -25,12 +26,13 @@ record ECDH256KeyPair(
             var publicKey = (ECPublicKey) keyPair.getPublic();
             var affineX = publicKey.getW().getAffineX().toByteArray();
             var affineY = publicKey.getW().getAffineY().toByteArray();
-            var rawPublicKey = new byte[PUBLIC_KEY_PART_LENGTH * 2 + 1];
+            var rawPublicKey = new byte[KEY_PART_LENGTH * 2 + 1];
             rawPublicKey[0] = 0x04;
-            System.arraycopy(affineX, affineX[0] == 0 ? 1 : 0, rawPublicKey, 1, PUBLIC_KEY_PART_LENGTH);
-            System.arraycopy(affineY, affineY[0] == 0 ? 1 : 0, rawPublicKey, PUBLIC_KEY_PART_LENGTH + 1, PUBLIC_KEY_PART_LENGTH);
+            System.arraycopy(affineX, affineX[0] == 0 ? 1 : 0, rawPublicKey, 1, KEY_PART_LENGTH);
+            System.arraycopy(affineY, affineY[0] == 0 ? 1 : 0, rawPublicKey, KEY_PART_LENGTH + 1, KEY_PART_LENGTH);
             var privateKey = (ECPrivateKey) keyPair.getPrivate();
-            var rawPrivateKey = privateKey.getS().toByteArray();
+            var encodedPrivateKey = privateKey.getS().toByteArray();
+            var rawPrivateKey = encodedPrivateKey.length == KEY_PART_LENGTH ? encodedPrivateKey : Arrays.copyOfRange(encodedPrivateKey, encodedPrivateKey.length - KEY_PART_LENGTH, encodedPrivateKey.length);
             return new ECDH256KeyPair(rawPublicKey, rawPrivateKey, publicKey, privateKey);
         }catch (NoSuchAlgorithmException exception) {
             throw new RuntimeException("Missing ECDH256 implementation", exception);
