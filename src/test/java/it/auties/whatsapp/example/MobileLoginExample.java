e7356bebe3b10304aa4d45077e76ec6399acdf86
Alessandro Autiero <alautiero@gmail.com>
2025-05-01 02:38:13 +0200

    Removed unnecessary properties from CompanionDevice Updated Medias to follow redirects Fixed Android version downloading Discovered that whatsapp dropped kaios support RIP(https://faq.whatsapp.com/420008397294796)

--- src/test/java/it/auties/whatsapp/example/MobileLoginExample.java
+++ src/test/java/it/auties/whatsapp/example/MobileLoginExample.java
@@ -2,8 +2,7 @@ package it.auties.whatsapp.example;
 
 import it.auties.whatsapp.api.Whatsapp;
 import it.auties.whatsapp.model.companion.CompanionDevice;
-import it.auties.whatsapp.model.signal.auth.UserAgent;
-import it.auties.whatsapp.util.AppMetadata;
+import it.auties.whatsapp.model.mobile.SixPartsKeys;
 
 import java.util.Scanner;
 
@@ -18,16 +17,12 @@ public class MobileLoginExample {
             case 2 -> false;
             default -> throw new IllegalStateException("Unexpected value: " + scanner.nextInt());
         };
-        AppMetadata.getVersion(UserAgent.PlatformType.KAIOS).join();
         Whatsapp.mobileBuilder()
-                .newConnection()
+                .newConnection(SixPartsKeys.of(sixParts))
                 // .proxy(URI.create("http://username:password@host:port/")) Remember to set an HTTP proxy
-                .device(CompanionDevice.android(business)) // Make sure to select the correct account type(business or personal) or you'll get error 401
-                .unregistered()
-                .verificationCodeSupplier(() -> "1")
-                .register(114141414141414L)
-                .join()
-                .whatsapp()
+                .device(CompanionDevice.ios(business)) // Make sure to select the correct account type(business or personal) or you'll get error 401
+                .registered()
+                .orElseThrow()
                 .addNodeReceivedListener(incoming -> System.out.printf("Received node %s%n", incoming))
                 .addNodeSentListener(outgoing -> System.out.printf("Sent node %s%n", outgoing))
                 .addLoggedInListener(api -> System.out.println("Logged in"))
